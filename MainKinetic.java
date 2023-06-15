4f029d95e4270b1790d0564b43183fe3b7b9129a
hendrawan1804 <136673806+hendrawan1804@users.noreply.github.com>
2023-06-15 11:52:02 +0700

    Add files via upload

---
+++ MainKinetic.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package w04_encapsulation;
+
+/**
+ *
+ * @author asus
+ */
+public class MainKinetic {
+    double massa, kecepatan;
+    
+
+    public double getMassa() {
+        return massa;
+    }
+
+    public void setMassa(double massa) {
+        this.massa = massa;
+    }
+
+    public double getKecepatan() {
+        return kecepatan;
+    }
+
+    public void setKecepatan(double kecepatan) {
+        this.kecepatan = kecepatan;
+    }
+    public double hitungEnergiKinetic(){
+        double energiKinetic = 0.5 * massa * kecepatan * kecepatan;
+        return energiKinetic;
+        
+    }
+        public static void main(String[] args) {
+            MainKinetic sepeda = new MainKinetic();
+            sepeda.setMassa(20);
+            sepeda.setKecepatan(2);
+            
+            double energiKinetic = sepeda.hitungEnergiKinetic();
+            
+            System.out.println(sepeda.getMassa());
+            System.out.println(sepeda.getKecepatan());
+            System.out.println(energiKinetic);
+   
+        }
+}
+        
+
